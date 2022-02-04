681bf8c2e138bccbf07a3797f5d2ef3e991a2ee6
Grace <grace@primetrust.com>
2022-02-03 22:29:43 -0800

    ?

--- lib/fireblocks/api/transactions.rb
+++ lib/fireblocks/api/transactions.rb
@@ -58,7 +58,7 @@ module Fireblocks
               id: source_id
             },
             headers: {}
-          }.merge(options, destination_params).compact
+          }.merge(options, destination_params, headers).compact
 
           create(body, headers)
         end
