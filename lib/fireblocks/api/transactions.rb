35c00be5f6423c39f403d00fb1a3d81d749914f7
Grace <grace@primetrust.com>
2022-02-03 22:32:21 -0800

    .

--- lib/fireblocks/api/transactions.rb
+++ lib/fireblocks/api/transactions.rb
@@ -22,7 +22,7 @@ module Fireblocks
           headers
         ].freeze
 
-        def create(options, headers: {})
+        def create(options, headers: nil)
           body = options.slice(*VALID_TRANSACTION_KEYS)
           Fireblocks::Request.post(body: body, path: '/v1/transactions', headers: headers)
         end
@@ -37,7 +37,7 @@ module Fireblocks
           one_time_address: nil,
           tag: nil,
           options: {},
-          headers: {}
+          headers: nil
         )
 
           destination_params = destination_payload(
@@ -47,7 +47,7 @@ module Fireblocks
             destination_id: destination_id,
             one_time_address: one_time_address,
             tag: tag,
-            headers: {}
+            headers: headers
           )
 
           body = {
