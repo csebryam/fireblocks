7c67af43ec92578c213e5b747d42cb61cc2f9d70
Grace <grace@primetrust.com>
2022-03-09 15:28:44 -0800

    introduce read_timeout config var to request

--- lib/fireblocks/api/request.rb
+++ lib/fireblocks/api/request.rb
@@ -43,6 +43,7 @@ module Fireblocks
 
     def post(body, headers = {})
       req = Net::HTTP::Post.new(uri)
+      req.read_timeout = Fireblocks.configuration.read_timeout_value
       request_headers(body, headers).each { |rk, rv| req[rk] = rv }
       req.body = body.to_json
 
